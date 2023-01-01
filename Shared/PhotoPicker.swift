55375789d767e7612dd373676f72f8969ade3022
Edgar Gavrik <edgar.gavrik@gmail.com>
2023-01-01 14:11:20 +0100

    starting to work on a photo picker

--- Shared/PhotoPicker.swift
+++ Shared/PhotoPicker.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct PhotoPicker: View {
-    @State private var text: String = ""
+    @State private var text = """
+\"Twelve significant photograps in any one year is a good crop\" \n --Ansel Adams"
+"""
     var body: some View {
         VStack {
-            Text("Hello, World!")
+            HStack{
+                Group {
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.left")
+                    })
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image(systemName: "camera.fill")
+                    })
+                }
+                .padding(30)
+                .font(.system(size: 30))
+            }
+            VStack {
+                if #available(iOS 16.0, *) {
+                    TextField("", text: $text, axis: .vertical)
+                        .frame(width: 400, height: 300, alignment: .topTrailing)
+                        .lineLimit(5...10)
+                        .padding()
+                        .font(.system(size: 27))
+                } else {
+                    #warning("Fallback: need to handle earlier versions")
+                }
+            }
+            .frame(alignment: .trailing)
+            Spacer()
         }
     }
 }
