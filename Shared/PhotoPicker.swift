f5e5ad1c80f9070b4f7b447eef02a1f6bdac3a4e
Edgar Gavrik <edgar.gavrik@gmail.com>
2023-01-01 15:26:18 +0100

    can upload a pic from the gallery now

--- Shared/PhotoPicker.swift
+++ Shared/PhotoPicker.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import PhotosUI
 
+@available(iOS 16.0, *)
 struct PhotoPicker: View {
-    @State private var text = """
-\"Twelve significant photograps in any one year is a good crop\" \n --Ansel Adams"
-"""
+    @State private var text = "\"Twelve significant photograps in any one year is a good crop\" \n --Ansel Adams\""
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var sItem: PhotosPickerItem? = nil
+    @State private var selectedImageData: Data? = nil
     var body: some View {
         VStack {
             HStack{
@@ -19,22 +22,32 @@ struct PhotoPicker: View {
                         Image(systemName: "chevron.left")
                     })
                     Spacer()
-                    Button(action: {}, label: {
+                    PhotosPicker(selection: $sItem, matching: .images) {
                         Image(systemName: "camera.fill")
-                    })
+                    }
+                    .onChange(of: sItem) { newItem in
+                        Task {
+                            if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                                selectedImageData = data
+                            }
+                        }
+                    }
                 }
                 .padding(30)
                 .font(.system(size: 30))
             }
+            
             VStack {
-                if #available(iOS 16.0, *) {
-                    TextField("", text: $text, axis: .vertical)
-                        .frame(width: 400, height: 300, alignment: .topTrailing)
-                        .lineLimit(5...10)
-                        .padding()
-                        .font(.system(size: 27))
-                } else {
-                    #warning("Fallback: need to handle earlier versions")
+                TextField("", text: $text, axis: .vertical)
+                    .frame(width: 400, height: 100, alignment: .topTrailing)
+                    .lineLimit(5...10)
+                    .padding()
+                    .font(.system(size: 27))
+                if let selectedImageData, let uiImage = UIImage(data: selectedImageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 350, height: 350)
                 }
             }
             .frame(alignment: .trailing)
@@ -45,6 +58,10 @@ struct PhotoPicker: View {
 
 struct PhotoPicker_Previews: PreviewProvider {
     static var previews: some View {
-        PhotoPicker()
+        if #available(iOS 16.0, *) {
+            PhotoPicker()
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
