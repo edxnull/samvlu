5b34b2d067880be8b73e54acc80e54f9c6b1a42f
Edgar Gavrik <edgar.gavrik@gmail.com>
2023-09-28 23:26:20 +0200

    check for ios 16.0 availability

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        SwipeToDismissKeyboard()
+        if #available(iOS 16.0, *) {
+            PhotoPicker()
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
 
